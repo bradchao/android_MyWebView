9c9c8de9e805bc15b055bd308335a2347a98cc43
Brad Chao <brad@brad.tw>
2016-09-20 10:44:39 +0800

    v1

---
+++ app/src/main/java/brad/tw/mywebview/MainActivity.java
@@ -0,0 +1,24 @@
+package brad.tw.mywebview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private TextView mesg;
+    private WebView webview;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mesg = (TextView)findViewById(R.id.mesg);
+        webview = (WebView)findViewById(R.id.webview);
+        initWebView();
+    }
+
+    private void initWebView(){
+
+    }
+}
